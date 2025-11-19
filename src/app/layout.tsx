cac6f81f0eb62a7bc30ae9bf0a90e540f3f1f68b
Shree Vigneshwara <vigneshwara204@gmail.com>
2025-11-19 18:26:20 +0000

    Can this Website Turn into PWA ?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: 'Recipe Right',
   description: 'Generate delicious recipes tailored to your needs!',
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({
