c455d64c453c19fac24bf207e1ce78cf3fb844d7
Code Kickoff <prithvipawar289@gmail.com>
2025-11-20 11:42:33 +0530

    Cook Right me

--- src/components/RecipeDisplay.tsx
+++ src/components/RecipeDisplay.tsx
@@ -44,14 +44,16 @@ export function RecipeDisplay({ recipe, originalRecipe, onTranslate, isTranslati
               className="rounded-lg"
             />
           ) : (
-            <Image
-              src="https://placehold.co/600x400.png"
-              alt={recipe.recipeName}
-              layout="fill"
-              objectFit="cover"
+            <iframe
               className="rounded-lg"
-              data-ai-hint="food recipe"
-            />
+              width="100%"
+              height="100%"
+              src={`https://www.youtube.com/embed?listType=search&list=${encodeURIComponent(recipe.recipeName + ' recipe')}`}
+              title="YouTube video player"
+              frameBorder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+              allowFullScreen
+            ></iframe>
           )}
         </div>
 
